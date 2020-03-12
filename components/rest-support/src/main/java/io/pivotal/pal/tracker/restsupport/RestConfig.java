7b21cad81f15f8ab4ade2a61f47361de490362e5
venkata Cognizant Git <venkatasivaramakrishna.nallagorla@cognizant.com>
2020-03-12 15:29:34 -0500

    changed components rest support

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -2,13 +2,14 @@ package io.pivotal.pal.tracker.restsupport;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class RestConfig {
-
+    @LoadBalanced
 
 
     @Bean
